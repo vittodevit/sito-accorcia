31dd10337c74e269b97a467b9dadf8e9fa36212b
Vittorio Lo Mele <hi@vitto.dev>
2025-05-30 20:08:04 +0200

    test api url change by env var

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {ApiConfigService} from './services/api-config.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected title = 'sito-accorcia-demo';
+
+  apiUrl: string = "";
+
+  constructor(
+    apiConfigService: ApiConfigService,
+  ) {
+    this.apiUrl = apiConfigService.getApiUrl();
+  }
 }
